88ed01ffaa22ddc52e6f10828619c34c2981fee7
xiaoxiong <1227372602@qq.com>
2018-08-21 10:56:13 +0800

    基础的socket操作

---
+++ src/main/java/com/test/demo/socketdemo1/SocketClient.java
@@ -0,0 +1,29 @@
+package com.test.demo.socketdemo1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * 客户端
+ */
+public class SocketClient {
+
+    public  static void main(String[] args){
+
+        try {
+            Socket client = new Socket("127.0.0.1",9999);
+            PrintWriter pw = new PrintWriter(client.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            pw.write(br.readLine());
+            pw.close();
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/main/java/com/test/demo/socketdemo1/SocketServer.java
@@ -0,0 +1,32 @@
+package com.test.demo.socketdemo1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 服务器端
+ */
+public class SocketServer {
+
+    public static void main(String[] args) {
+
+        try {
+            ServerSocket server = new ServerSocket(9999);
+            while (true){
+                //接收消息
+                Socket socket = server.accept();
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                System.out.println("server accept msg :"+br.readLine());
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
